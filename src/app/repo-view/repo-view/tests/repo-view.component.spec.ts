ada4d9d9a4f2846854ed9e83edac06446388d19f
Wallace Coelho <wac.almeida@gmail.com>
2017-11-07 15:38:21 -0200

    Refactor RepoView Tests

--- src/app/repo-view/repo-view/tests/repo-view.component.spec.ts
+++ src/app/repo-view/repo-view/tests/repo-view.component.spec.ts
@@ -1,76 +1,53 @@
-// import { async, ComponentFixture, TestBed, fakeAsync, inject, tick } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { HttpModule, XHRBackend, Response, ResponseOptions } from '@angular/http';
-// import { MockBackend } from '@angular/http/testing';
-// import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
-// import { MarkdownModule } from 'angular2-markdown';
-// import { Subject } from 'rxjs/Subject';
-// import { ActivatedRoute, Params } from '@angular/router';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
+import { MarkdownModule } from 'angular2-markdown';
 
-// import { RepoViewComponent } from '../repo-view.component';
-// import { RepoViewService } from '../../repo-view-service/repo-view.service';
-// import {
-//   getMock, PARAM_REPO, PARAM_USERNAME, NAME_SELECTOR, STARS_SELECTOR, LANGUAGE_SELECTOR,
-//   EXTERNAL_LINK_SELECTOR, DESCRIPTION_SELECTOR,
-// } from './repo-view.test-options';
+import { RepoViewComponent } from '../repo-view.component';
+import { RepoViewService } from '../../repo-view-service/repo-view.service';
+import { COMPONENTS_SELECTORS } from './repo-view.test-options';
+import { repoMock } from '../../repo-view-service/repoMock';
 
 
-// describe('RepoViewComponent', () => {
-//   let component: RepoViewComponent;
-//   let fixture: ComponentFixture<RepoViewComponent>;
-//   let params: Subject<Params>;
+describe('RepoViewComponent', () => {
+  let component: RepoViewComponent;
+  let fixture: ComponentFixture<RepoViewComponent>;
+  let repoViewService;
 
-//   beforeEach(async(() => {
-//     params = new Subject<Params>();
-//     TestBed.configureTestingModule({
-//       imports: [
-//         Angular2FontawesomeModule,
-//         HttpModule,
-//         MarkdownModule,
-//         RouterTestingModule,
-//       ],
-//       declarations: [
-//         RepoViewComponent
-//       ],
-//       providers: [
-//         RepoViewService,
-//         { provide: XHRBackend, useClass: MockBackend },
-//       ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        Angular2FontawesomeModule,
+        HttpModule,
+        MarkdownModule,
+        RouterTestingModule,
+      ],
+      declarations: [
+        RepoViewComponent
+      ],
+      providers: [
+        RepoViewService,
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RepoViewComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RepoViewComponent);
+    component = fixture.componentInstance;
+    repoViewService = fixture.debugElement.injector.get(RepoViewService);
+    spyOn(repoViewService, 'getRepoData').and.returnValue(repoMock);
+    spyOn(repoViewService, 'getRepoReadme').and.returnValue('### REPO WORKS');
+    fixture.detectChanges();
+  });
 
-//   it('should have basic info',
-//     fakeAsync(inject([XHRBackend], (mockBackend) => {
-//       mockBackend.connections.subscribe((connection) => {
-//         connection.mockRespond(new Response(new ResponseOptions({
-//           body: getMock(connection.request.url)
-//         })));
-//       });
-//       params.next({
-//         repoOwner: PARAM_USERNAME,
-//         repoName: PARAM_REPO,
-//       });
-//       tick();
-//       fixture.detectChanges();
-
-//       const componentList = [
-//         NAME_SELECTOR,
-//         // STARS_SELECTOR,
-//         // LANGUAGE_SELECTOR,
-//         // EXTERNAL_LINK_SELECTOR,
-//         // DESCRIPTION_SELECTOR
-//       ];
-//       componentList.forEach((componentSelector) => {
-//         const componentHtml = fixture.nativeElement.querySelector(componentSelector);
-//         expect(componentHtml).toBeTruthy();
-//       });
-//     })
-//   ));
-// });
+  it('should have basic info', () => {
+    fixture.whenStable().then(() => {
+      COMPONENTS_SELECTORS.forEach((selector) => {
+        const htmlComponent = fixture.nativeElement.querySelector(selector);
+        expect(htmlComponent).toBeTruthy();
+      });
+    });
+  });
+});
