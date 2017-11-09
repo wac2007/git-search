05c438b81d6af4baa46b5fb8d7ef47d47227863c
Wallace Coelho <wac.almeida@gmail.com>
2017-11-09 15:46:05 -0200

    Fix responsiveness in UserProfile

--- src/app/user-view/user-profile/tests/user-profile.test-options.ts
+++ src/app/user-view/user-profile/tests/user-profile.test-options.ts
@@ -3,10 +3,10 @@ import { userMock } from '../../../shared/mocks/userMock';
 
 export const USER_PROFILE_MOCK = userMock;
 
-export const USERNAME_SELECTOR = '.profile__title';
-export const USER_PROFILE_LINK_SELECTOR = '.profile__url';
-export const FOLLOWERS_SELECTOR = '.profile__info[title=Followers]';
-export const FOLLOWING_SELECTOR = '.profile__info[title=Following]';
-export const EMAIL_SELECTOR = '.profile__info[title=Email]';
-export const BIO_SELECTOR = '.profile__bio';
-export const AVATAR_SELECTOR = '.profile__avatar';
+export const USERNAME_SELECTOR = '.profile-title';
+export const USER_PROFILE_LINK_SELECTOR = '.profile-url';
+export const FOLLOWERS_SELECTOR = '.profile-info[title=Followers]';
+export const FOLLOWING_SELECTOR = '.profile-info[title=Following]';
+export const EMAIL_SELECTOR = '.profile-info[title=Email]';
+export const BIO_SELECTOR = '.profile-bio';
+export const AVATAR_SELECTOR = '.profile-avatar';
