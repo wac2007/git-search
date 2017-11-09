5d1b7bc116b67318c35298d55646dbf40104f77c
Wallace Coelho <wac.almeida@gmail.com>
2017-11-09 19:59:32 -0200

    Remove unused scss file in RadioGroup

--- src/app/shared/components/radio-group/radio-group.component.ts
+++ src/app/shared/components/radio-group/radio-group.component.ts
@@ -5,8 +5,7 @@ import { RadioItem } from './radio-item';
 
 @Component({
   selector: 'app-radio-group',
-  templateUrl: './radio-group.component.html',
-  styleUrls: ['./radio-group.component.scss']
+  templateUrl: './radio-group.component.html'
 })
 export class RadioGroupComponent {
   @Input() radioItems: Array<RadioItem>;
