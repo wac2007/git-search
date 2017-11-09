655fd1adcb82c58054f842e817edf348816e90ef
Wallace Coelho <wac.almeida@gmail.com>
2017-11-09 16:35:26 -0200

    Remove unused css from UserRepos

--- src/app/user-view/user-repos/user-repos.component.ts
+++ src/app/user-view/user-repos/user-repos.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-repos',
-  templateUrl: './user-repos.component.html',
-  styleUrls: ['./user-repos.component.scss']
+  templateUrl: './user-repos.component.html'
 })
 export class UserReposComponent {
   @Input() repoList: Array<Object>;
